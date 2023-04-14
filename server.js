81e877e337f5d72ddb155d7f6d1af9a0e3026069
Kimberly Brown <kbrown1@unc.edu>
2023-04-14 12:16:24 -0400

    updates to server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+import { rps } from "./lib/rpsls.js";
+import { rpsls } from "./lib/rpsls.js";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/app/", (req, res) => {
+    res.status(200).send('200 OK');
+})
+
+app.get("/app/rps/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps));
+})
+
+app.get("/app/rpsls/", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls));
+})
+
+app.get("/app/rps/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+})
+
+app.get("/app/rpsls/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+})
+
+app.post("/app/rps/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+})
+
+app.post("/app/rpsls/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
